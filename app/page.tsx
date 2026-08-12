65ef72bc722342ed052f70aa75efb5270be1876c
Soumya Bhate <56678035+soumyabhate19@users.noreply.github.com>
2026-08-11 22:06:00 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="hidden shrink-0 sm:block sm:-mr-10">
+            <div className="hidden shrink-0 sm:block sm:-mr-20">
               <Floral layout="static" size="lg" photoSrc="/profile.jpg" />
             </div>
           </div>
