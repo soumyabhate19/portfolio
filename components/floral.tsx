8122d4edaa8208f8e0b4950a7c7ad94ba2f9a565
Soumya Bhate <56678035+soumyabhate19@users.noreply.github.com>
2026-08-11 21:47:53 -0400

    Update floral.tsx

--- components/floral.tsx
+++ components/floral.tsx
@@ -10,8 +10,8 @@ const positions = {
 };
 
 const sizeMap = {
-  sm: "h-[300px] w-[300px]",
-  lg: "h-[380px] w-[380px] lg:h-[440px] lg:w-[440px]",
+  sm: "h-[400px] w-[400px]",
+  lg: "h-[450px] w-[450px] lg:h-[500px] lg:w-[500px]",
 };
 
 export default function FloralFlourish({
